c790216e63466d09b172dd9d5b2e891d53e9bf1c
namhun <namhundred@naver.com>
2022-04-17 17:55:38 +0900

    x button revise
    x button revise

--- src/server/kakaoaddress.js
+++ src/server/kakaoaddress.js
@@ -1,11 +1,12 @@
 function closeDaumPostcode(element_layer) {
     // element_layer.parentElement.style.display = 'none';
     element_layer.parentElement.style.height = '0%';
+    element_layer.innerHTML = "";
 }
 function sample2_execDaumPostcode(element_layer) {
-    console.log(element_layer)
+
     new daum.Postcode({
-        oncomplete: function(data) {
+        oncomplete: function (data) {
             var addr = ''; // 주소 변수
             var extraAddr = ''; // 참고항목 변수
             if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
@@ -13,41 +14,38 @@ function sample2_execDaumPostcode(element_layer) {
             } else { // 사용자가 지번 주소를 선택했을 경우(J)
                 addr = data.jibunAddress;
             }
-            if(data.userSelectedType === 'R'){
+            if (data.userSelectedType === 'R') {
 
-                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
                     extraAddr += data.bname;
                 }
-                if(data.buildingName !== '' && data.apartment === 'Y'){
+                if (data.buildingName !== '' && data.apartment === 'Y') {
                     extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
                 }
-                if(extraAddr !== ''){
+                if (extraAddr !== '') {
                     extraAddr = ' (' + extraAddr + ')';
                 }
 
-                document.getElementById(element_layer.parentElement.className).innerHTML = addr+extraAddr +`<img class="arrow" src="./src/assets/svg/arrow.svg">`;
+                document.getElementById(element_layer.parentElement.className).innerHTML = addr + extraAddr + `<img class="arrow" src="./src/assets/svg/arrow.svg">`;
 
             } else {
                 document.getElementById(element_layer).value = '';
             }
 
-            // document.getElementById('sample2_postcode').value = data.zonecode;
-            // document.getElementById("sample2_address").value = addr;
-            // document.getElementById("sample2_detailAddress").focus();
-
-            // element_layer.parentElement.style.display = 'none';
             element_layer.parentElement.style.height = '0%';
         },
-        width : '100%',
-        height : '100%',
-        maxSuggestItems : 5
+        width: '100%',
+        height: '100%',
+        maxSuggestItems: 5
     }).embed(element_layer);
-    // element_layer.parentElement.style.display = 'block';
     element_layer.parentElement.style.height = '100%';
+    const x = document.createElement('div');
+    x.innerHTML = `<img src="//t1.daumcdn.net/postcode/resource/images/close.png" id="btnCloseLayer"    onclick="closeDaumPostcode(${element_layer.id})" >`;
+    element_layer.appendChild(x).classList.add('remove');
     initLayerPosition(element_layer);
 }
 
-function initLayerPosition(element_layer){
+function initLayerPosition(element_layer) {
 
     element_layer.style.width = '100%';
     element_layer.style.height = '100%';
