77ea1ebb1ecc434fcfd391f2f433562188d8f5b5
Namheon Baek <namhundred@naver.com>
2022-04-21 10:29:32 +0900

    map list detail add
    map list detail add

--- src/components/maplist.js
+++ src/components/maplist.js
@@ -11,6 +11,41 @@ export default class Maplist extends HTMLElement {
         <div class="blank">
         </div>
         </div>
+        <div class="delivery">
+        <ul class="delivery__list">
+        <li class="seleted delivery__item">
+            배달
+        </li>
+        <li class="delivery__item">
+            포장
+        </li>
+        </ul>
+        </div>
+        <div class="category">
+        <ul class="menu__board">
+        <li class="menu__item">
+        돈까스 회 일식
+        </li>
+        <li class="menu__item">
+        중식
+        </li>
+        <li class="menu__item">
+        치킨
+        </li>
+        <li class="menu__item">
+        백반 죽 국수
+        </li>
+        <li class="menu__item">
+        카페 디저트
+        </li>
+        <li class="menu__item">
+        분식
+        </li>
+        <li class="menu__item">
+        찜 탕 찌개
+        </li>
+        </ul>
+        </div>
         `;
         this.querySelector('.arrow').onclick = e => this.parentElement.classList.remove('open');
     }
