3b90c1b7853b7b637154b1dbf784b4e4bb684588
Namheon Baek <namhundred@naver.com>
2022-05-12 22:52:20 +0900

    test:add firebase data console.log in search.js

--- src/components/search.js
+++ src/components/search.js
@@ -1,14 +1,27 @@
+import { db } from "../server/firebase.js";
 export default class Search extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
         <div class="maphead">
         <img class="arrow remove" src="./src/assets/svg/leftarrow.svg">
         <input class="search__store">
-
+        <img class="temp" src="./src/assets/svg/leftarrow.svg">
         </div>
-        `
-        this.querySelector(".arrow").onclick = (e) =>
-            this.parentElement.classList.remove("open");
-    }
+        `;
+    this.querySelector(".arrow").onclick = (e) =>
+      this.parentElement.classList.remove("open");
+    this.querySelector(".temp").onclick = (e) => {
+      const keyword = this.querySelector("input").value;
+      db.collection("store")
+        .orderBy("name")
+        .startAt("스")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+          });
+        });
+    };
+  }
 }
-customElements.define('search-wrap', Search);
+customElements.define("search-wrap", Search);
