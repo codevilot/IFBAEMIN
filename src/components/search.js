91d42ecdaee5d3aca41b819d3ce50807c1f7fb6b
Namheon Baek <namhundred@naver.com>
2022-05-13 19:48:56 +0900

    feat:add search result in search.js

--- src/components/search.js
+++ src/components/search.js
@@ -7,18 +7,57 @@ export default class Search extends HTMLElement {
         <input class="search__store">
         <img class="temp" src="./src/assets/svg/leftarrow.svg">
         </div>
+        <div class="stores__wrap">
+        <div class="stores">
+        </div>
+        </div>
         `;
     this.querySelector(".arrow").onclick = (e) =>
       this.parentElement.classList.remove("open");
     this.querySelector(".temp").onclick = (e) => {
+      const stores = this.querySelector(".stores");
+      stores.innerHTML = ``;
       const keyword = this.querySelector("input").value;
       db.collection("store")
         .orderBy("name")
-        .startAt("스")
+        .startAt(keyword)
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-            console.log(doc.data());
+            console.log(doc);
+
+            const div = document.createElement("div");
+            const img = `https://github.com/codevilot/IFBAEMIN/blob/main/src/assets/store/${
+              doc.data().thumbnail || doc.data().name
+            }?raw=true`;
+            stores.appendChild(div);
+            div.classList.add("store__box");
+            div.innerHTML = `
+
+            <img class="store__img" src=${img}>
+                    <div class="store__info">
+                    <div class="store__name">${doc.data().name}${
+              doc.data().coupon ? `<span class="coupon">쿠폰</span>` : ""
+            }</div> 
+                    <div>⭐ ${
+                      doc.data().score
+                    } <span class="store__inner__menu">${
+              doc.data().menu
+            }</span></div>
+                    ${
+                      doc.data().least || doc.data().fee
+                        ? `<div>${
+                            doc.data().least
+                              ? "최소주문" + doc.data().least
+                              : ""
+                          } ${
+                            doc.data().fee ? ", 배달팁" + doc.data().fee : ""
+                          } </div>`
+                        : ""
+                    }
+                    <div class="store__time">🕒 ${doc.data().time}</div>
+                    </div> 
+                    `;
           });
         });
     };
