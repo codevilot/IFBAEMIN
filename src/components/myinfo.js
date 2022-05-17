5d3185dec92980c07b080e3f4a962e49ee4adc8d
Namheon Baek <namhundred@naver.com>
2022-05-17 17:00:23 +0900

    refactor:add firebase user info update in myinfo.js

--- src/components/myinfo.js
+++ src/components/myinfo.js
@@ -2,12 +2,33 @@ import { auth } from "../server/firebase.js";
 export default class Myinfo extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
+      <div class="maphead">
+      <img class="arrow remove" src="./src/assets/svg/leftarrow.svg">
+      <h3 class="title">
+      내 정보 수정
+      </h3>
+      <div class="update">
+      저장
+      </div>
+      </div>
       <input value="${
         !auth.currentUser.displayName
           ? auth.currentUser.email
           : auth.currentUser.displayName
-      }"/>
+      }" class="nameInput"/>
+    
       `;
+
+    this.querySelector(".update").onclick = (e) => {
+      this.displayName = this.querySelector(".nameInput").value;
+      auth.currentUser
+        .updateProfile({
+          displayName: this.displayName,
+        })
+        .then(() => {
+          console.log("Update your name", this.displayName);
+        });
+    };
   }
 }
 customElements.define("myinfo-container", Myinfo);
